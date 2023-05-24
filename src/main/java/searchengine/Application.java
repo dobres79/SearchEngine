54f3645c8e289a1c1b6f498e924cce69c920490a
Nikita <dobres@yabndex.ru>
2023-05-24 10:03:20 +0300

    Тест

--- src/main/java/searchengine/Application.java
+++ src/main/java/searchengine/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        //TODO сделать диплом
         SpringApplication.run(Application.class, args);
     }
 }
